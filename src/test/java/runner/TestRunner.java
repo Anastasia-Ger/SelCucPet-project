f685ab7e68e1c85d045c040f397d104680b84c0a
Anastasia-Ger <163204996+Anastasia-Ger@users.noreply.github.com>
2025-04-23 23:03:59 +0200

    Testrunner change

--- src/test/java/runner/TestRunner.java
+++ src/test/java/runner/TestRunner.java
@@ -6,11 +6,13 @@ import org.junit.platform.suite.api.IncludeEngines;
 import org.junit.platform.suite.api.SelectClasspathResource;
 import org.junit.platform.suite.api.Suite;
 
+import static io.cucumber.core.options.Constants.GLUE_PROPERTY_NAME;
 import static io.cucumber.junit.platform.engine.Constants.PLUGIN_PROPERTY_NAME;
 
 @Suite //Junit test suite
 @IncludeEngines("cucumber") //cucumber tests will be executed in Junit test suite
 @SelectClasspathResource("features") //cucum.tests will be available in the features folder under src/test/resources
+@ConfigurationParameter(key = GLUE_PROPERTY_NAME, value = "steps")
 @ConfigurationParameter(key = PLUGIN_PROPERTY_NAME, value="json:target/out.json")
 public class  TestRunner {
     //empty class
